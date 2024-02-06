db01e491c8e4ba02a2c80be1b9f93ec633deac26
Federica Chiara Spelta <federica.c.spelta@accenture.com>
2024-02-06 13:20:35 +0100

    fixed configuration for start application

--- library-book-project-be/src/main/java/com/example/librarybookproject/LibraryBookProjectApplication.java
+++ library-book-project-be/src/main/java/com/example/librarybookproject/LibraryBookProjectApplication.java
@@ -2,9 +2,10 @@ package com.example.librarybookproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+
+@EnableEurekaServer
 @SpringBootApplication
 public class LibraryBookProjectApplication {
 
