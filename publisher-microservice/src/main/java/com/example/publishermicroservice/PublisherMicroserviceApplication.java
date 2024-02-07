a6da45a541b817d3f9093b8c7a9b356b83a7a188
ConnieConnie1 <costanza.gorick@gmail.com>
2024-02-07 17:06:30 +0100

    publisher

---
+++ publisher-microservice/src/main/java/com/example/publishermicroservice/PublisherMicroserviceApplication.java
@@ -0,0 +1,16 @@
+package com.example.publishermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+public class PublisherMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherMicroserviceApplication.class, args);
+	}
+
+}
