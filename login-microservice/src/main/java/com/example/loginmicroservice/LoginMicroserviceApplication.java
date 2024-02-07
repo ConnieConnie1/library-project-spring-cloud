7a9fa167e7e99d5af3f7f16b775767e36e67047a
ConnieConnie1 <costanza.gorick@gmail.com>
2024-02-07 15:45:10 +0100

    aggiunto servizio login

---
+++ login-microservice/src/main/java/com/example/loginmicroservice/LoginMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.loginmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LoginMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginMicroserviceApplication.class, args);
+	}
+
+}
