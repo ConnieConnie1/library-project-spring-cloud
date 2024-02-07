2cca7a752d4215397aed4d4f68c436317a8af9ed
ConnieConnie1 <costanza.gorick@gmail.com>
2024-02-07 15:16:23 +0100

    modified name microservice

--- book-microservice/src/main/java/com/library/bookMicroservice/BookMicroserviceApplication.java
+++ book-microservice/src/main/java/com/library/bookMicroservice/BookMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.library.firstMicroservice;
+package com.library.bookMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class FirstMicroserviceApplication {
+public class BookMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstMicroserviceApplication.class, args);
+		SpringApplication.run(BookMicroserviceApplication.class, args);
 	}
 
 }
---
+++ library-book-project-fe/src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const Banner = () => {
+  return (
+    <div>
+        <h1>Migliaia di libri in offerta</h1>
+        <h2>Occasioni da non perdere assolutamente</h2>
+        <Button>Approfittane ora</Button>
+    </div>
+  )
+}
+
+export default Banner;
