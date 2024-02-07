6b184198e19f5273f68a02003361dde9c085a377
Federica Chiara Spelta <federica.c.spelta@accenture.com>
2024-02-07 15:53:14 +0100

    fixed ecommerce microservice

--- ecommerce-microservice/src/main/java/com/library/ecommerceMicroservice/EcommerceMicroserviceApplication.java
+++ ecommerce-microservice/src/main/java/com/library/ecommerceMicroservice/EcommerceMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.library.secondMicroservice;
+package com.library.ecommerceMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SecondMicroserviceApplication {
+public class EcommerceMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecondMicroserviceApplication.class, args);
+		SpringApplication.run(EcommerceMicroserviceApplication.class, args);
 	}
 
 }
