6d26533ceb26461e37e3b635fdc0b82b065b01fd
ConnieConnie1 <costanza.gorick@gmail.com>
2024-02-08 17:46:12 +0100

    banner inizio sezione

--- library-book-project-fe/src/App.js
+++ library-book-project-fe/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner'; 
+import Section1 from './components/Section1';
+
 
 function App() {
   return (
     <div className="App">
     <NavBar />
     <Banner />
+    <Section1/>
     </div>
   );
 }
