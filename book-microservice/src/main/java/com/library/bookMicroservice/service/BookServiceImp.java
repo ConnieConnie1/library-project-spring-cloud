de251c183d353e9f8d856dc2731841b1dc4e70d2
ConnieConnie1 <costanza.gorick@gmail.com>
2024-02-08 10:48:27 +0100

    Book record

--- book-microservice/src/main/java/com/library/bookMicroservice/service/BookServiceImp.java
+++ book-microservice/src/main/java/com/library/bookMicroservice/service/BookServiceImp.java
@@ -1,12 +1,18 @@
 package com.library.bookMicroservice.service;
 
+import com.library.bookMicroservice.record.BookRecord;
+import com.library.bookMicroservice.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.List;
 
 public class BookServiceImp implements BookService{
 
     // Inietto la repository
+    @Autowired
+    private BookRepository bookRepository;
     @Override
-    public List<BookDTO> getAllBooks() {
-        return bookRepo.findAll();
+    public List<BookRecord> getAllBooks() {
+        return bookRepository.findAll();
     }
 }
