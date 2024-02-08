78c9c9336bda9b53bd5216ab042a2cb103eaf976
ConnieConnie1 <costanza.gorick@gmail.com>
2024-02-08 09:59:12 +0100

    Controller Book-service fix

--- book-microservice/src/main/java/com/library/bookMicroservice/controller/BookController.java
+++ book-microservice/src/main/java/com/library/bookMicroservice/controller/BookController.java
@@ -2,10 +2,11 @@ package com.library.bookMicroservice.controller;
 
 import jakarta.ws.rs.Path;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Path("/book")
+@RequestMapping("/book")
 public class BookController {
 
     @GetMapping("/test")
