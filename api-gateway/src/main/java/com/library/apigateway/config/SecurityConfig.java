ca024b85fce2f4715d30e13bc6aef8b92d00999f
Federica Chiara Spelta <federica.c.spelta@accenture.com>
2024-02-14 16:00:08 +0100

    added token

--- api-gateway/src/main/java/com/library/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/library/apigateway/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.library.apigateway.config.jwt.JwtAuthenticationFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 
 @Configuration
@@ -13,8 +14,9 @@ public class SecurityConfig  {
     public FilterRegistrationBean<JwtAuthenticationFilter> jwtFilter() {
         FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new JwtAuthenticationFilter());
-        registrationBean.addUrlPatterns("/api/books/*");
-        registrationBean.addUrlPatterns("/api/authors/*");// Specifica i percorsi delle richieste che devono passare attraverso il filtro JWT
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registrationBean.addUrlPatterns("/api/books*");
+        registrationBean.addUrlPatterns("/api/authors*");// Specifica i percorsi delle richieste che devono passare attraverso il filtro JWT
         return registrationBean;
     }
 
