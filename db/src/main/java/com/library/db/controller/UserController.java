a0e4dbaf5a7b96a9e40157ddd81c698529607426
Federica Chiara Spelta <federica.c.spelta@accenture.com>
2024-02-20 14:37:19 +0100

    create publisher entity and repository

--- db/src/main/java/com/library/db/controller/UserController.java
+++ db/src/main/java/com/library/db/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.library.db.controller;
 
-import com.library.db.entity.User;
+import com.library.db.entity.RegisteredUser;
 import com.library.db.record.UserRecord;
 import com.library.db.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +14,12 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/createUser")
-    public User createUser(@RequestBody UserRecord userRecord){
+    public RegisteredUser createUser(@RequestBody UserRecord userRecord){
         return userService.createNewUser(userRecord);
     }
 
     @GetMapping("login")
-    public User login(@RequestParam String mail, @RequestParam String password){
+    public RegisteredUser login(@RequestParam String mail, @RequestParam String password){
         return userService.getUserByEmailAndPassword(mail, password);
     }
 }
---
+++ db/src/main/java/com/library/db/entity/Publisher.java
@@ -0,0 +1,42 @@
+package com.library.db.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="PUBLISHER")
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "PUBLISHER_NAME")
+    private String publisherName;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
