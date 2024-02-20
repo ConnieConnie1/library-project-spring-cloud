3028eb9a4eeff8f65c4b99beccfb247a74e81ef2
Federica Chiara Spelta <federica.c.spelta@accenture.com>
2024-02-20 11:51:11 +0100

    added security config

---
+++ api-gateway/src/main/java/com/library/apigateway/config/GatewayConfig.java
@@ -0,0 +1,33 @@
+package com.library.apigateway.config;
+
+import com.library.apigateway.filter.AuthFilter;
+import com.library.apigateway.record.AuthorRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Autowired
+    AuthFilter authFilter;
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        // adding 2 rotes to first microservice as we need to log request body if method is POST
+        return builder.routes()
+                .route("author-microservice:8085",r -> r.path("/api/authors/**")
+                        .and().method("GET")
+                        .and().readBody(AuthorRecord.class, s -> true).filters(f -> f.filters(authFilter))
+                        .uri("lb://AUTHOR-MICROSERVICE"))
+                .build();
+    }
+
+
+    /*@Bean
+    public WebFilter responseFilter(){
+        return new PostGlobalFilter();
+    }*/
+
+}
---
+++ api-gateway/src/main/java/com/library/apigateway/record/AuthorRecord.java
@@ -0,0 +1,4 @@
+package com.library.apigateway.record;
+
+public record AuthorRecord(Long id, String name, String surname, Long genreId, String biography) {
+}
