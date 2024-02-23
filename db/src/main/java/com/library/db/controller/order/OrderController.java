337ea5051ce390d92c22a64c57e6f7fb3bd348bd
ConnieConnie1 <costanza.gorick@gmail.com>
2024-02-23 15:16:12 +0100

    Order nel db

--- db/src/main/java/com/library/db/controller/order/OrderController.java
+++ db/src/main/java/com/library/db/controller/order/OrderController.java
@@ -1,6 +1,7 @@
 package com.library.db.controller.order;
 
 import com.library.db.entity.order.Orders;
+import com.library.db.record.PaginationResponse;
 import com.library.db.service.order.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +12,25 @@ import java.util.Optional;
 @RequestMapping("/order")
 public class OrderController {
 
-        @Autowired
-        private OrderService orderService;
+    @Autowired
+    private OrderService orderService;
 
-        @GetMapping("/{orderId}")
-        public Orders getOrderById(@PathVariable("orderId") Long orderId){
-            return orderService.getOrderById(orderId);
-        }
-
-        @DeleteMapping("/delete/{orderId}")
-        public void deleteOrder (@PathVariable("orderId") Long orderId){
-           orderService.deleteOrderById(orderId);
-        }
+    @GetMapping
+    PaginationResponse<Orders> getAllOrders(
+            @RequestParam(required = false) Integer orderNumber,
+            @RequestParam(required = true) Long userId,
+            @RequestParam(required = false) Integer currentPage,
+            @RequestParam(required = false) Integer pageSize) {
+        return orderService.getAllOrders(orderNumber, userId, currentPage, pageSize);
     }
+
+    @GetMapping("/{orderId}")
+    public Orders getOrderById(@PathVariable("orderId") Long orderId) {
+        return orderService.getOrderById(orderId);
+    }
+
+    @DeleteMapping("/delete/{orderId}")
+    public void deleteOrder(@PathVariable("orderId") Long orderId) {
+        orderService.deleteOrderById(orderId);
+    }
+}
---
+++ db/src/main/java/com/library/db/repository/order/OrderRepositoryImpl.java
@@ -0,0 +1,77 @@
+package com.library.db.repository.order;
+
+import com.library.db.entity.author.Author;
+import com.library.db.entity.book.Book;
+import com.library.db.entity.book.Orders;
+import com.library.db.entity.genre.Genre;
+import com.library.db.entity.order.Orders;
+import com.library.db.entity.publisher.Publisher;
+import com.library.db.entity.user.Users;
+import com.library.db.record.PaginationResponse;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
+import org.springframework.data.domain.Pageable;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class OrderRepositoryImpl implements OrderCustomRepository {
+    @PersistenceContext
+    EntityManager em;
+
+    @Override
+    public PaginationResponse<Orders> findOrdersByFilter(Pageable pageable, Integer orderNumber, Long userId) {
+        PaginationResponse<Orders> response = new PaginationResponse<Orders>();
+
+        final int currentPageNumber = pageable.getPageNumber();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+
+        // Per il libro
+        CriteriaQuery<Orders> cq = cb.createQuery(Orders.class);
+        Root<Orders> root = cq.from(Orders.class);
+        List<Predicate> predicates = getPredicates(cb, root, orderNumber, userId);
+
+        cq.where(predicates.stream().toArray(Predicate[]::new));
+        cq.orderBy(cb.asc(root.get("orderNumber"))); // Ordinati in ordine di numero ordine
+
+        TypedQuery<Orders> query = em.createQuery(cq);
+        query.setFirstResult((pageable.getPageNumber() - 1) * pageable.getPageSize());
+        query.setMaxResults(pageable.getPageSize());
+        List<Orders> result = query.getResultList();
+
+        // Per la count
+        CriteriaQuery<Long> cqCount = cb.createQuery(Long.class);
+        Root<Orders> rootCount = cqCount.from(Orders.class);
+        List<Predicate> predicateCount = getPredicates(cb, rootCount, orderNumber, userId);
+        cqCount.select(cb.count(rootCount));
+        cqCount.where(predicateCount.stream().toArray(Predicate[]::new));
+        Long orderCount = em.createQuery(cqCount).getSingleResult();
+
+        int totalPage = (int) Math.ceil((double) orderCount / pageable.getPageSize());
+        // PageSize verrà passato da FE.
+        response.setData(result);
+        response.setTotalPage(totalPage);
+        response.setCurrentPage(currentPageNumber);
+
+        return response;
+    }
+
+    private List<Predicate> getPredicates(CriteriaBuilder cb, Root<Orders> root, Integer orderNumber, Long userId) {
+        List<Predicate> predicates = new ArrayList<>();
+        Join<Orders, Book> bookJoin = root.join("books");
+
+        if (orderNumber != null) {
+            predicates.add(cb.equal(root.get("orderNumber"), orderNumber));
+        }
+
+        if (userId != null) {
+            predicates.add(cb.equal(bookJoin.get("userId"), userId));
+        }
+        return predicates;
+
+    }
+}
