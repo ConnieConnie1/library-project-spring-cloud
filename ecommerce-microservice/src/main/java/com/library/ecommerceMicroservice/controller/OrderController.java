b6a8c782b70a94deef55138d752474577155370b
ConnieConnie1 <costanza.gorick@gmail.com>
2024-02-23 11:31:56 +0100

    getAllOrders in microservice e-commerce

--- ecommerce-microservice/src/main/java/com/library/ecommerceMicroservice/controller/OrderController.java
+++ ecommerce-microservice/src/main/java/com/library/ecommerceMicroservice/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.library.ecommerceMicroservice.controller;
 
 import com.library.ecommerceMicroservice.entity.Orders;
 import com.library.ecommerceMicroservice.record.OrderRecord;
+import com.library.ecommerceMicroservice.record.PaginationResponse;
 import com.library.ecommerceMicroservice.service.OrderService;
 import jakarta.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
@@ -16,6 +21,16 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @GetMapping("")
+    public PaginationResponse<Orders> getAllOrders(
+            @RequestParam(required = false) Integer orderNumber,
+            @RequestParam(required = true) Long userId,
+            @RequestParam(required = false, defaultValue = "1") Integer currentPage,
+            @RequestParam(required = false, defaultValue = "10") Integer pageSize) {
+        return orderService.getAllOrders(orderNumber, userId, currentPage, pageSize);
+
+    }
 
     @GetMapping("/{id}")
     public Response getOrderById(@PathVariable Long id) {
